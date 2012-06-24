99535ff646741560633c5153279af159d00b1d06
Joe Dallago <jd.dallago@gmail.com>
2012-06-24 20:04:39 +0100

    Perfected the tiling system to be more accurate on resize, as well as added some basic components to posts.

--- clusterflunk/static/js/wireframes.js
+++ clusterflunk/static/js/wireframes.js
@@ -1,19 +1,24 @@
 $(function() {
-    var width = 242;
+    var width = 232;
+    var gutterWidth = 10;
 
-    $('.posts').masonry({
-        itemSelector : '.post',
-        columnWidth : width
-    });
-
-    $(window).resize(function() {
-        var columns = Math.floor( $(window).width() / width );
-        var actual_width = columns * width;
+    var resize_it = function() {
+        var columns = Math.floor( ($(window).width() + 10) / (width + gutterWidth) );
+        var actual_width = (columns * (width + gutterWidth)) - gutterWidth;
         if ($(".header").width() != actual_width) {
             $(".header").width(actual_width);
         }
         if ($(".posts").width() != actual_width) {
             $(".posts").width(actual_width);
         }
+    };
+
+    resize_it();
+    $('.posts').masonry({
+        itemSelector : '.post',
+        columnWidth : width,
+        gutterWidth: gutterWidth
     });
+
+    $(window).resize(resize_it);
 });
--- clusterflunk/templates/posts/posts.mako
+++ clusterflunk/templates/posts/posts.mako
@@ -4,7 +4,10 @@
     <div class="posts">
         % for i in range(25):
         <div class="post">
-            <div class="inner"></div>
+            <a class="title" href="#">This is a title</a>
+            <div class="description">
+                This is a description of epic proportions.
+            </div>
         </div>
         % endfor
     </div>
